b016563d672e8a90a1fccd681bf023b6a2733535
Ant Newman <antnewman@Ants-MacBook-Pro.local>
2020-12-18 20:51:36 +0000

    update

--- src/components/form/CustomForm.js
+++ src/components/form/CustomForm.js
@@ -47,6 +47,7 @@ export default function CustomForm() {
                             setFileId(null)
                             setTargetLanguageCode(null)
                             setTargetLanguageName(null)
+                            console.log('reset')
                         })
                 }
             })
